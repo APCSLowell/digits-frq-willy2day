62632fa717ef04bf7a61249b86557ff3adbe61c3
willy2day <143228693+willy2day@users.noreply.github.com>
2024-04-15 22:17:23 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,22 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	digitList = new ArrayList <Integer>();
+	    for (int i=num; i>0;i/10){
+		    digitList.add(num%10);
+		    
+	    }
+		returns digitList;
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+		for (int i=0; i<digitList.size(); i++){
+			if (digitList.get(i)>digitList.get(i+1))
+				return false;
+	
+		}
+	return true;
 	}
 	
 	public String toString()
